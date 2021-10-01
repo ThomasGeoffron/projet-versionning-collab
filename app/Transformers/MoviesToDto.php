ce9a577d3ce774a403d789d8ba387c57b995c454
Thomas GEOFFRON <tgeoffron@myges.fr>
2021-10-01 11:54:55 +0200

    Endpoint API
    Retour d'un timestamp UNIX pour l'endpoint de l'API
    
    Closes #24

--- app/Transformers/MoviesToDto.php
+++ app/Transformers/MoviesToDto.php
@@ -1,1 +1,16 @@
-<?php namespace App\Transformers;use App\Models\Movies;class MoviesToDto{public static function provideTransformer(Movies $movie){switch($movie->type){case'Comedy':return['id'=>$movie->id,'name'=>$movie->name,'type'=>'Comedy / Drama',];default:return['id'=>$movie->id,'name'=>$movie->name,'type'=>$movie->type,];case'Mystery/Crime':return['id'=>$movie->id,'name'=>$movie->name,'type'=>'Mystery / Crime',];default:return['id'=>$movie->id,'name'=>$movie->name,'type'=>$movie->type,];}}}
+<?php namespace App\Transformers;
+use App\Models\Movies;
+class MoviesToDto{
+    public static function provideTransformer(Movies $movie){
+        switch($movie->type){
+            case'Comedy':
+                return['id'=>$movie->id,'name'=>$movie->name,'type'=>'Comedy / Drama',];
+            default:
+                return['id'=>$movie->id,'name'=>$movie->name,'type'=>$movie->type,];
+            case'Mystery/Crime':
+                return['id'=>$movie->id,'name'=>$movie->name,'type'=>'Mystery / Crime',];
+            default:
+                return['id'=>$movie->id,'name'=>$movie->name,'type'=>$movie->type,];
+        }
+    }
+}
